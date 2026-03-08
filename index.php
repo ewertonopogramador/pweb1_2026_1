f05f26204b3991b8e09482d5cf86892f5172aff3
ewertonopogramador <ewertondsdl@gmail.com>
2026-03-08 15:55:24 -0300

    Add files via upload
    Exercicios propostos

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link href="Styles/Principal.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <h1>Ewerton dos Santos de Lima</h1>
+  <div>
+    <a href="matricula.html" class="button-1"> Formulário de matrícula de alunos em uma escola</a>
+    <a href="fichacadastral.html" class="button-2">Ficha Cadastral</a>
+    <a href="listadeinscritos.html" class="button-3">Lista de inscritos</a>
+  </div>
+</body>
+
+</html>
