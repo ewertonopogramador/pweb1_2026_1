8842e2ad6a498c3eccab44dd640252c1d723c123
ewertonopogramador <ewertondsdl@gmail.com>
2026-05-04 17:19:00 -0300

    commit formulario
    mais atualizações

--- php/aulas/aula03_formulario.php
+++ php/aulas/aula03_formulario.php
@@ -6,19 +6,19 @@
     <h3>Fomulário Aula</h3>
     <div class="col-6">
         <label for="nome">Nome</label>
-        <input type="text" name ="nome" calss="form-control">
+        <input type="text" name ="nome" class="form-control">
     </div>
     <div class="col-6">
         <label for="email">Email</label>
-        <input type="text" name ="email" calss="form-control">
+        <input type="text" name ="email" class="form-control">
     </div>
     <div class="col-6">
         <label for="telefone">Telefone</label>
-        <input type="number" name ="telefone" calss="form-control">
+        <input type="number" name ="telefone" class="form-control">
     </div>
     <div class="col-6">
         <label for="senha">Senha</label>
-        <input type="password" name ="senha" calss="form-control">
+        <input type="password" name ="senha" class="form-control">
     </div>
     <div class="mt-2">
         <button type="submit" class="btn btn-primary">Salvar</button>
